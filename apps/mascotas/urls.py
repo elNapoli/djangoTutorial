b2801ec550d0d39b8730e77186dab59603b813c9
Baldomero Napoli <baldomero.napoli@gmail.com>
2018-01-20 15:23:10 -0300

    crud realizado con clases y no con definiciones

--- apps/mascotas/urls.py
+++ apps/mascotas/urls.py
@@ -1,10 +1,10 @@
 from django.conf.urls import url, include
-from apps.mascotas.views import index, mascota_view, mascotas_list, mascota_edit, mascota_delete
+from apps.mascotas.views import index, mascota_view, mascotas_list, mascota_edit, mascota_delete,MascotaList, MascotaCreate, MascotaUpdate, MascotaDelete
 app_name = 'mascotas'
 urlpatterns = [
     url(r'^$', index, name='index'),
-    url(r'^nuevo/', mascota_view, name='mascota_crear'),
-    url(r'^listar/', mascotas_list, name='mascota_listar'),
-    url(r'^editar/(?P<id_mascota>\d+)/', mascota_edit, name='mascota_editar'),
-    url(r'^eliminar/(?P<id_mascota>\d+)/', mascota_delete, name='mascota_eliminar'),
+    url(r'^nuevo/', MascotaCreate.as_view(), name='mascota_crear'),
+    url(r'^listar/', MascotaList.as_view(), name='mascota_listar'),
+    url(r'^editar/(?P<pk>\d+)/', MascotaUpdate.as_view(), name='mascota_editar'),
+    url(r'^eliminar/(?P<pk>\d+)/', MascotaDelete.as_view(), name='mascota_eliminar'),
 ]
--- apps/mascotas/views.py
+++ apps/mascotas/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render,redirect
 from apps.mascotas.forms import MascotasForm, Mascotas
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 # Create your views here.
 
 def index(request):
@@ -37,5 +39,28 @@ def mascota_delete(request, id_mascota):
         mascota.delete()
         return redirect('mascotas:mascota_listar')
     return render(request, 'mascotas/mascota_delete.html', {'mascota': mascota})
+
+class MascotaList(ListView):
+    model = Mascotas
+    template_name ='mascotas/mascota_list.html'
+
+class MascotaCreate(CreateView):
+    model = Mascotas
+    form_class = MascotasForm
+    template_name = 'mascotas/mascotas_form.html'
+    success_url = reverse_lazy('mascotas:mascota_listar')
+
+class MascotaUpdate(UpdateView):
+    model = Mascotas
+    form_class = MascotasForm
+    template_name = 'mascotas/mascotas_form.html'
+    success_url = reverse_lazy('mascotas:mascota_listar')
+
+
+class MascotaDelete(DeleteView):
+    model = Mascotas
+    template_name = 'mascotas/mascota_delete.html'
+    success_url = reverse_lazy('mascotas:mascota_listar')
+
 
 
